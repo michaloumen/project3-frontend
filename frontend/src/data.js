33ce173545f19e0ef1e09cfd03ee8b450735b9e7
michaloumen <michelle.lmendonca@gmail.com>
2021-05-29 16:56:42 -0300

    inicializando homepage com react

---
+++ frontend/src/data.js
@@ -0,0 +1,46 @@
+export default {
+    products: [
+        {
+            name: "Jogo Clínico Colorido",
+            category: "Clínica Geral",
+            image: "/images/jogoClinico.jpg",
+            price: "80,00",
+            brand: "Golgran"
+        },
+        {
+            name: "Fita para Autoclave",
+            category: "Biossegurança",
+            image: "/images/fitaAutoclave.jpg",
+            price: "7,00",
+            brand: "Cremer"
+        },
+        {
+            name: "Lima K 1ª Série",
+            category: "Endodontia",
+            image: "/images/limaKPrimeiraSérie.jpg",
+            price: "25,90",
+            brand: "Golgran"
+        },
+        {
+            name: "Sugador Endodôntico Descartável",
+            category: "Descartáveis",
+            image: "/images/sugadorEndodonticoDescartavel.jpg",
+            price: "14,90",
+            brand: "SSPlus"
+        },
+        {
+            name: "Kit Perio",
+            category: "Periodontia",
+            image: "/images/kitPerio.jpg",
+            price: "229,09",
+            brand: "Millennium - Golgran"
+        },
+        {
+            name: "Lima Wave One Gold Reciprocante",
+            category: "Endodontia",
+            image: "/images/waveOneGold.jpg",
+            price: "284,31",
+            brand: "Maillefer - Densply Sirona"
+        }
+    ]
+}
